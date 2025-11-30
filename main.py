20fb050b9d5ecd4b9bf81d810ccc2aee7623c941
hermanni-fernandes <hermanni.fernandes@gmail.com>
2025-11-30 16:01:08 -0300

    Rekisteröity ProductsController FastAPI-sovellukseen

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from controllers.products_controller import router as products_router
 
 app = FastAPI()
 
+app.include_router(products_router)
+
 @app.get("/")
 def root():
     return {"message": "FastAPI toimii!"}
-
-app.include_router(products_router)
